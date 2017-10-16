bd83f0880dd2d8156d4db184f1f9465265153a56
aangelo96 <aangelo@student.unimelb.edu.au>
2017-10-16 22:17:00 +1100

    Adding more test to ARActivityTesting

--- arcane/app/src/test/java/nimbus/arcane/ARActivityTesting.java
+++ arcane/app/src/test/java/nimbus/arcane/ARActivityTesting.java
@@ -82,7 +82,7 @@ public class ARActivityTesting {
     }
 
     @Test
-    public void deltaAngleTest() throws Exception {
+    public void deltaAngleTest1() throws Exception {
         ARActivity sampleActivity = new ARActivity();
 
         Location location1 = Mockito.mock(Location.class);
@@ -94,6 +94,51 @@ public class ARActivityTesting {
         Mockito.when(location2.getLongitude()).thenReturn(144.5);
 
         assertEquals((141.631-90),sampleActivity.calculatedAngle(90,location1.getLatitude(), location1.getLongitude(), location2.getLatitude(), location2.getLongitude()), 3);
+    }
+
+    @Test
+    public void deltaAngleTest2() throws Exception {
+        ARActivity sampleActivity = new ARActivity();
+
+        Location location1 = Mockito.mock(Location.class);
+        Location location2 = Mockito.mock(Location.class);
+
+        Mockito.when(location1.getLatitude()).thenReturn(-37.7964);
+        Mockito.when(location1.getLongitude()).thenReturn(144.9612);
+        Mockito.when(location2.getLatitude()).thenReturn(-37.8108);
+        Mockito.when(location2.getLongitude()).thenReturn(144.9631);
+
+        assertEquals((174.04891184209873-90),sampleActivity.calculatedAngle(90,location1.getLatitude(), location1.getLongitude(), location2.getLatitude(), location2.getLongitude()), 3);
+    }
+
+    @Test
+    public void deltaAngleTest3() throws Exception {
+        ARActivity sampleActivity = new ARActivity();
+
+        Location location1 = Mockito.mock(Location.class);
+        Location location2 = Mockito.mock(Location.class);
+
+        Mockito.when(location1.getLatitude()).thenReturn(-37.810068);
+        Mockito.when(location1.getLongitude()).thenReturn(144.964106);
+        Mockito.when(location2.getLatitude()).thenReturn(-37.818214);
+        Mockito.when(location2.getLongitude()).thenReturn(144.967936);
+
+        assertEquals((159.624-90),sampleActivity.calculatedAngle(90,location1.getLatitude(), location1.getLongitude(), location2.getLatitude(), location2.getLongitude()), 3);
+    }
+
+    @Test
+    public void deltaAngleTest4() throws Exception {
+        ARActivity sampleActivity = new ARActivity();
+
+        Location location1 = Mockito.mock(Location.class);
+        Location location2 = Mockito.mock(Location.class);
+
+        Mockito.when(location1.getLatitude()).thenReturn(39.099912);
+        Mockito.when(location1.getLongitude()).thenReturn(-94.581213);
+        Mockito.when(location2.getLatitude()).thenReturn(38.627089);
+        Mockito.when(location2.getLongitude()).thenReturn(-90.200203);
+
+        assertEquals((96.51-90),sampleActivity.calculatedAngle(90,location1.getLatitude(), location1.getLongitude(), location2.getLatitude(), location2.getLongitude()), 3);
     }
 
 }
