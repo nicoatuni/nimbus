80c1ce8a715284396f327146758b0aaaff529766
Nico <ndinata@student.unimelb.edu.au>
2017-10-17 16:43:33 +1100

    Add feature to leave from a group

--- arcane/app/src/main/java/nimbus/arcane/ChatsFragment.java
+++ arcane/app/src/main/java/nimbus/arcane/ChatsFragment.java
@@ -1,10 +1,12 @@
 package nimbus.arcane;
 
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
@@ -22,16 +24,22 @@ import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -41,24 +49,14 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 public class ChatsFragment extends Fragment {
 
-    private RelativeLayout mGroupList;
     private RecyclerView mGroupsList;
 
     private View mMainView;
 
-    private View mLayout;
-    private TextView mGroupName;
-    private TextView mStatus;
-    private CircleImageView mImageView;
-
     private FirebaseUser mCurrentUser;
     private DatabaseReference mRootRef;
-    private DatabaseReference mGroupRef;
     private DatabaseReference mGroupsDatabase;
     private DatabaseReference mAllGroupsDatabase;
-
-    private Iterable<DataSnapshot> mGroups;
-    private String group_id;
 
     public ChatsFragment() {
         // Required empty public constructor
@@ -66,7 +64,7 @@ public class ChatsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-              Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mMainView = inflater.inflate(R.layout.fragment_chats, container, false);
 
@@ -75,7 +73,6 @@ public class ChatsFragment extends Fragment {
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
 
         mRootRef = FirebaseDatabase.getInstance().getReference();
-        mGroupRef = mRootRef.child("Groups");
 
         mAllGroupsDatabase = mRootRef.child("Groups");
         mAllGroupsDatabase.keepSynced(true);
@@ -104,60 +101,68 @@ public class ChatsFragment extends Fragment {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
 
-                        final String groupName = dataSnapshot.child("name").getValue().toString();
-                        String groupStatus = dataSnapshot.child("status").getValue().toString();
-                        String groupThumbImage = dataSnapshot.child("thumb_image").getValue().toString();
+                        if (dataSnapshot.exists()) {
+                            final String groupName = dataSnapshot.child("name").getValue().toString();
+                            String groupStatus = dataSnapshot.child("status").getValue().toString();
+                            String groupThumbImage = dataSnapshot.child("thumb_image").getValue().toString();
 
-                        chatsViewHolder.setName(groupName);
-                        chatsViewHolder.setDisplayImage(groupThumbImage, getContext());
-                        chatsViewHolder.setStatus(groupStatus);
+                            chatsViewHolder.setName(groupName);
+                            chatsViewHolder.setDisplayImage(groupThumbImage, getContext());
+                            chatsViewHolder.setStatus(groupStatus);
 
-                        chatsViewHolder.mView.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
+                            chatsViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
 
-                                CharSequence options[] = new CharSequence[]{"Open Profile", "Open Chat", "Add to Group"};
+                                    CharSequence options[] = new CharSequence[]{"Open Profile", "Open Chat", "Add to Group", "Leave Group"};
 
-                                final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                                    final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 
-                                builder.setTitle("Select Options");
-                                builder.setItems(options, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                    builder.setTitle("Select Options");
+                                    builder.setItems(options, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
 
-                                        // Click Event for each item
-                                        if (i == 0) {
+                                            // Click Event for each item
+                                            if (i == 0) {
 
-                                            Intent profileIntent = new Intent(getContext(), GroupProfileActivity.class);
-                                            profileIntent.putExtra("group_id", list_group_id);
-                                            startActivity(profileIntent);
+                                                Intent profileIntent = new Intent(getContext(), GroupProfileActivity.class);
+                                                profileIntent.putExtra("group_id", list_group_id);
+                                                startActivity(profileIntent);
 
+                                            }
+
+                                            if (i == 1) {
+
+                                                Intent chatGroupIntent = new Intent(getContext(), GroupChatActivity.class);
+                                                chatGroupIntent.putExtra("group_id", list_group_id);
+                                                chatGroupIntent.putExtra("group_name", groupName);
+                                                startActivity(chatGroupIntent);
+
+                                            }
+
+                                            if (i == 2) {
+
+                                                Intent addToGroupIntent = new Intent(getContext(), AddToGroupActivity.class);
+                                                addToGroupIntent.putExtra("group_id", list_group_id);
+                                                addToGroupIntent.putExtra("group_name", groupName);
+                                                startActivity(addToGroupIntent);
+
+                                            }
+
+                                            if (i == 3) {
+
+                                                leaveGroup(list_group_id, groupName);
+
+                                            }
                                         }
+                                    });
 
-                                        if (i == 1) {
+                                    builder.show();
 
-                                            Intent chatGroupIntent = new Intent(getContext(), GroupChatActivity.class);
-                                            chatGroupIntent.putExtra("group_id", list_group_id);
-                                            chatGroupIntent.putExtra("group_name", groupName);
-                                            startActivity(chatGroupIntent);
-
-                                        }
-
-                                        if (i == 2) {
-
-                                            Intent addToGroupIntent = new Intent(getContext(), AddToGroupActivity.class);
-                                            addToGroupIntent.putExtra("group_id", list_group_id);
-                                            addToGroupIntent.putExtra("group_name", groupName);
-                                            startActivity(addToGroupIntent);
-
-                                        }
-                                    }
-                                });
-
-                                builder.show();
-
-                            }
-                        });
+                                }
+                            });
+                        }
 
                     }
 
@@ -205,5 +210,28 @@ public class ChatsFragment extends Fragment {
             Picasso.with(ctx).load(thumb_image).placeholder(R.drawable.default_avatar).into(userImage_view);
 
         }
+    }
+
+    private void leaveGroup(String group_id, final String group_name) {
+
+        String user_id = mCurrentUser.getUid();
+
+        Map leaveGroupMap = new HashMap();
+        leaveGroupMap.put("Groups/" + group_id + "/Members/" + user_id, null);
+        leaveGroupMap.put("Users/" + user_id + "/Groups/" + group_id, null);
+
+        mRootRef.updateChildren(leaveGroupMap).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+
+                if (task.isSuccessful()) {
+
+                    Toast.makeText(getContext(), "You left the group " + group_name, Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+
     }
 }
