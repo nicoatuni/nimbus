53ea4d4657e1053149ba7d130fe4eebc22706f79
coetomo <christopherhadioetomo@gmail.com>
2017-10-19 20:00:11 +1100

    settings activity unit testing

--- arcane/app/src/test/java/nimbus/arcane/SettingsActivityTesting.java
+++ arcane/app/src/test/java/nimbus/arcane/SettingsActivityTesting.java
@@ -1,57 +1,59 @@
 package nimbus.arcane;
 
-import android.app.Application;
-import android.location.Location;
-import android.test.ApplicationTestCase;
-import android.widget.Button;
-import android.widget.TextView;
+import android.app.Activity;
+import android.content.Intent;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.w3c.dom.Text;
 
 import java.lang.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import de.hdodenhof.circleimageview.CircleImageView;
 
-import static junit.framework.Assert.assertEquals;
-
-/**
- * Created by Richard Aldrich on 10/10/2017.
- */
 @RunWith(MockitoJUnitRunner.class)
 
 public class SettingsActivityTesting {
 
-    private TextView displayName;
-    private TextView displayStatus;
-    private CircleImageView displayImage;
+    @Mock
+    private DatabaseReference mockedDatabaseReference;
 
-    private Button imageBtn;
-    private Button statusBtn;
+    private StorageReference mockImageStorage;
 
-    private DatabaseReference mRootRef;
+    private SettingsActivity mockSettingClass;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
 
-        mRootRef = FirebaseDatabase.getInstance().getReference();
+        mockedDatabaseReference = Mockito.mock(DatabaseReference.class);
 
-//        displayName = (TextView) R.id.settings_display_name;
-//
-//        srcPostCode.setText("3055");
-//        destPostCode.setText("3010");
-//
-//        UI.addWidget("CALCULATE_BUTTON", new Button());
-//        UI.addWidget("COST_LABEL", costLabel);
-//        UI.addWidget("SOURCE_POST_CODE", srcPostCode);
-//        UI.addWidget("DESTINATION_POST_CODE", destPostCode);
+        FirebaseDatabase mockedFirebaseDatabase = Mockito.mock(FirebaseDatabase.class);
+        when(mockedFirebaseDatabase.getReference()).thenReturn(mockedDatabaseReference);
+
+        mockSettingClass = Mockito.mock(SettingsActivity.class);
+        mockSettingClass.setDataBase(mockedFirebaseDatabase.getReference());
     }
 
+
+    @Test
+    public void testOnActivityResult() {
+        int mockSuccessRequestCode = CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE;
+        int mockSuccessResultCode = Activity.RESULT_OK;
+        Intent mockData = new Intent();
+
+        mockSettingClass.onActivityResult(mockSuccessRequestCode, mockSuccessResultCode, mockData);
+
+        verify(mockSettingClass).onActivityResult(mockSuccessRequestCode,
+                mockSuccessResultCode, mockData);
+
+    }
 }
